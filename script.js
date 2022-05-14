310912200a4526d18b067c62f52747cb5e4b928c
RodrigoA97 <rodrigo97wd@gmail.com>
2022-05-14 10:18:15 -0300

    Logic+Buttons

--- script.js
+++ script.js
@@ -1,28 +1,87 @@
 const btnNumber = document.querySelectorAll(".num");
 const btnOperator = document.querySelectorAll(".operator");
 const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const del = document.querySelector("#delete");
+const dot = document.querySelector(".dot");
 const displayBot = document.querySelector("#displayBot");
 const displayTop = document.querySelector("#displayTop");
 
-
-let a=0;
-let b=0;
-
+let a = "";
+let b = "";
+let operator = "";
 
 btnNumber.forEach((button) =>
   button.addEventListener("click", () => displayNum(button.textContent))
 );
 
-clear.addEventListener("click", () => 
-  (displayBot.textContent = ""),
-  (displayTop.textContent = "")
+btnOperator.forEach((button) =>
+  button.addEventListener("click", () => setValues(button.textContent))
 );
+
+equals.addEventListener("click", () => result(displayBot.textContent));
+dot.addEventListener("click", addDot);
+del.addEventListener("click", deleteLast);
+clear.addEventListener("click", clearDisplay);
 
 function displayNum(num) {
   displayBot.textContent += num;
 }
 
+function setValues(button) {
+  if (displayBot.textContent == "") {
+    return;
+  } else {
+    a = displayBot.textContent;
+    operator = button;
+    displayTop.textContent = displayBot.textContent + button;
+    displayBot.textContent = "";
+  }
+}
+
+function result(value) {
+  if (operator == "") {
+    return;
+  } else {
+    b = value;
+    displayTop.textContent = displayTop.textContent + value + "=";
+    let result = operate(a, b, operator);
+    if (result > 999999) {
+      result=result.toExponential();
+    }
+    displayBot.textContent = result;
+    a = result;
+    operator = ""
+  }
+}
+
+function addDot() {
+  if (displayBot.textContent.includes(".")) {
+    return;
+  } else if (displayBot.textContent != "") {
+    displayBot.textContent += dot.textContent;
+  }
+}
+
+function deleteLast() {
+  let temp;
+  temp = displayBot.textContent;
+  temp = temp.substring(0, temp.length - 1);
+  displayBot.textContent = temp;
+}
+
+function clearDisplay() {
+  displayTop.textContent = "";
+  displayBot.textContent = "";
+  a = "";
+  b = "";
+  operator = "";
+}
+
+
 function add(a, b) {
+  a = Number(a);
+  b = Number(b);
   return a + b;
 }
 
